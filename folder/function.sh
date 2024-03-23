ee62a46c35fbefaff3dfbede1e7c3e8fd6f76dd3
agn3y <agneypatel96@gmail.com>
2024-03-23 23:22:12 +0530

    new folder storing practise shell scripts

---
+++ folder/function.sh
@@ -0,0 +1,20 @@
+#1st method
+#function funcname(){
+#sleep 10
+#echo “first way writing function”
+#}
+#funcname
+
+#2nd method
+#secondmethod(){
+#sleep 2
+#echo "second method for calling function"
+#}
+#secondmethod
+
+#3rd way to use function
+#function thirdme {
+#sleep "3"
+#echo "this is the third way"
+#}
+#thirdme
